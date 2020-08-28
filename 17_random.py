5a6edb1b92fb71a4b6f8c469a3114acf34b4166d
Isabel Sedunko <isabel.sedunko@gmail.com>
2020-08-28 14:12:03 +0200

    define fourtoseven function

--- 17_random.py
+++ 17_random.py
@@ -1,3 +1,11 @@
 import random
 for i in range (10):
     print(random.random())
+
+def fourtoseven():
+    # the end result of 4to7 formula
+    return 4*random.random()+3
+
+for i in range (10):
+    print (fourtoseven())
+    
